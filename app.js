56120b194845027c97ebc287d3f523157926f0e5
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-17 22:41:18 +0000

    Remove task function for multiple items #3

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/removetask', function(req, res){
         task.splice(task.indexOf(completeTask), 1);
     }
     else if(typeof completeTask === "object"){
-        for (var i; i < completeTask.length; i++ ){
+        for (var i=0; i < completeTask.length; i++ ){
         complete.push(completeTask[i]);
         task.splice(task.indexOf(completeTask), 1);
         }
