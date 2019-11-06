592921431cc2c56a54510551f970639f793283fa
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-06 00:09:18 +0000

    Adding React #2

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+//Write Function to do fetch
+
+
+render(){
+
+    return (
+        <h1>Everyone hates React!</h1>
+    );
+}
+
+
+}
+export default Home;
